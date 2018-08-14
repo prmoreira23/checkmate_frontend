081e35e7b1cad7a96368e78a616498084dafdc1c
prmoreira23 <prm.gredes@gmail.com>
2018-08-14 09:02:05 -0400

    Update dashboard to show incoming and outcoming contracts

--- src/components/ui/Dashboard.js
+++ src/components/ui/Dashboard.js
@@ -11,29 +11,58 @@ const Dashboard = () => {
         </div>
       </h2>
 
+      <h2>Incoming Contracts</h2>
       <table class="ui selectable inverted table">
         <thead>
           <tr>
-            <th>Name</th>
+            <th>Contract's title</th>
             <th>Status</th>
-            <th class="right aligned">Notes</th>
+            <th class="right aligned">Author</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>John</td>
-            <td>Approved</td>
-            <td class="right aligned">None</td>
+            <td>Home Lease Contract</td>
+            <td>Waiting for other party's signature</td>
+            <td class="right aligned">Patrick Abejar</td>
           </tr>
           <tr>
-            <td>Jamie</td>
-            <td>Approved</td>
-            <td class="right aligned">Requires call</td>
+            <td>Honda CRV 2015 Selling Contract</td>
+            <td>Binded</td>
+            <td class="right aligned">Jathmel Gordon</td>
           </tr>
           <tr>
-            <td>Jill</td>
-            <td>Denied</td>
-            <td class="right aligned">None</td>
+            <td>Business Lease Contract</td>
+            <td>Requires your signature</td>
+            <td class="right aligned">Alicia Keys</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Outcoming Contracts</h2>
+      <table class="ui selectable inverted table">
+        <thead>
+          <tr>
+            <th>Contract's title</th>
+            <th>Status</th>
+            <th class="right aligned">Recipient</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Home Lease Contract</td>
+            <td>Waiting for other party's signature</td>
+            <td class="right aligned">Patrick Abejar</td>
+          </tr>
+          <tr>
+            <td>Honda CRV 2015 Selling Contract</td>
+            <td>Binded</td>
+            <td class="right aligned">Jathmel Gordon</td>
+          </tr>
+          <tr>
+            <td>Business Lease Contract</td>
+            <td>Requires your signature</td>
+            <td class="right aligned">Frankie Cosmic</td>
           </tr>
         </tbody>
       </table>
